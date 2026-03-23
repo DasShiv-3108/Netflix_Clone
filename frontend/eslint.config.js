7fd61810040653a72d2adffb00527ec293c9862f
DasShiv-3108 <dasshiv3108@gmail.com>
2026-03-23 17:23:09 +0000

    fix: delete .eslintrc & creat eslint.config.js

---
+++ frontend/eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      "no-var": "error",
+      "semi": ["error", "always"]
+    }
+  }
+];
