8c16e0384afe48ee6e8538570b4b3eca288d3ea8
DasShiv-3108 <dasshiv3108@gmail.com>
2026-03-25 05:20:35 +0000

    fix: add ./ in code linting

---
+++ backend/eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      "no-var": "error",
+      "semi": ["error", "always"]
+    }
+  }
+];
+
